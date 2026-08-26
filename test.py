f6861000f64abd673d648aea89ad711d3a370a41
malodev <leadermushio377@gmail.com>
2026-08-26 13:06:53 +0200

    udpate course

---
+++ test.py
@@ -0,0 +1,6 @@
+x = 3 
+
+if x > 3 : 
+   print("x is greater than 3")
+else :
+   print("x is less than or equal to 3")
--- varaibles.py
+++
@@ -1,6 +0,0 @@
-x = 'hello'
-y = 'world'
-z=3
-print(x,y,z,sep="-",end=" ")
-print(type(x))
-print(type(y))
