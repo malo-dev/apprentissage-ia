5c1812a4667653f4a2537a1caf0ee42321644ef2
malodev <leadermushio377@gmail.com>
2026-08-26 12:14:35 +0200

    fix files managers

---
+++ output.py
@@ -0,0 +1,9 @@
+print("hello leader",end=" ")
+print("hello world")
+
+print("hello world ",30,40,50,sep="-")
+
+print("hello leader",end=" ")
+print("hello world")
+
+print("hello world ",30,40,50,sep="-")
